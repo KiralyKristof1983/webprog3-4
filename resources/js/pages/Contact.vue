a68e7e0a0d40b5958e7902d83d1aad8b97639781
Kristof Kiraly <kiralykristof2018@gmail.com>
2022-12-17 18:07:24 +0100

    Kapcsolat oldal fejlesztése

--- resources/js/pages/Contact.vue
+++ resources/js/pages/Contact.vue
@@ -1,3 +1,60 @@
 <template>
-    <h1>Kapcsolat</h1>
+<section id="contact-us">
+        <h1>Lépjen kapcsolatba</h1>
+
+
+        <div class="container">
+          <div class="contact-info">
+            <div class="specific-info">
+              <i class="fas fa-home"></i>
+              <div>
+                <p class="title">Fuvaros u. 12.</p>
+                <p class="subtitle">Nagykáta</p>
+              </div>
+            </div>
+            <div class="specific-info">
+              <i class="fas fa-phone-alt"></i>
+              <div>
+                <a href="">+36304650408 </a>
+
+
+                <p class="subtitle">Nyitvatartás: Hétfő - Péntek 6:00-12:00</p>
+              </div>
+            </div>
+            <div class="specific-info">
+              <i class="fas fa-envelope-open-text"></i>
+              <div>
+                <a href="mailto:kiralykristof2018@gmail.com">
+                  <p class="title">kiralykristof2018@gmail.com</p>
+                </a>
+                <p class="subtitle">Érdeklődjön!</p>
+              </div>
+            </div>
+          </div>
+
+          
+          <div class="contact-form">
+            <form action="" method="">
+              
+              <label for="name"><span>Név</span></label>
+              <input type="text" id="name" name="name" value="" />
+
+              
+              <label for="email"><span>E-mail</span></label>
+              <input type="text" id="email" name="email" value="" />
+
+              
+              <label for="subject"><span>Tárgy</span></label>
+              <input type="text" id="Subject" name="subject" value="" />
+
+
+              <label for="message"><span>Üzenet</span></label>
+              <textarea id="message" name="message"></textarea>
+
+
+              <input type="submit" value="Elküld" />
+            </form>
+          </div>
+        </div>
+      </section>
 </template>
